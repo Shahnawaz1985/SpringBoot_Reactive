d4f01eab94e47ae27d6747de264f1f856a90efb3
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-03-01 12:26:29 +0530

    Controller updated for default mapping.

--- src/main/java/com/eric/spring/boot/reactive/controller/HomeController.java
+++ src/main/java/com/eric/spring/boot/reactive/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.eric.spring.boot.reactive.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
 
-	Mono<String> home(){
+	@GetMapping(value = "/")
+	public Mono<String> home(){
 		return Mono.just("home");
 	}
 }
