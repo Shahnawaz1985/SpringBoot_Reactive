1a1d6b4c872889ee5ae269f2fdbd0cd9e8def21f
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-03-18 21:18:54 +0530

    Home Controller commented replaced with RouterFunctions.

--- src/main/java/com/eric/spring/boot/reactive/controller/HomeController.java
+++ src/main/java/com/eric/spring/boot/reactive/controller/HomeController.java
@@ -1,20 +1,18 @@
-package com.eric.spring.boot.reactive.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import reactor.core.publisher.Mono;
-
-@RestController
-//@RequestMapping(value = "/")
-public class HomeController {
-
-	//@RequestMapping(method = RequestMethod.GET)
-	@GetMapping(value = "/")
-	public Mono<String> home(){
-		return Mono.just("home.html");
-	}
-}
+/*
+ * package com.eric.spring.boot.reactive.controller;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * import reactor.core.publisher.Mono;
+ * 
+ * @RestController //@RequestMapping(value = "/") public class HomeController {
+ * 
+ * //@RequestMapping(method = RequestMethod.GET)
+ * 
+ * @GetMapping(value = "/") public Mono<String> home(){ return
+ * Mono.just("home.html"); } }
+ */
